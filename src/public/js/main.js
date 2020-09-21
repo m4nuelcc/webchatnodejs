7656dc205896c6168198270634955d6834bbc7a7
Manuel <manuelcarabiac@gmail.com>
2020-09-21 17:52:07 +0200

    añadido usuarios y para que se vea quien escribe

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,14 +1,37 @@
+//ESTA ES LA PARTE DEL CLIENTE
 $(function () {
 
   const socket = io();
 
-  //Obteniendo los elementos del DOM desde la interfact
+//Obteniendo los elementos del DOM desde la interfact
 const $messageform = $('#message-form');
 const $messageBox = $('#message');
 const $chat = $('#chat');
 
-// Eventos
+// Obteniendo Elementos del nicknameForm
+const $nickForm = $('#nickForm');
+const $nickError = $('#nickError');
+const $nickname = $('#nickname');
 
+const $users = $('#usernames');
+
+$nickForm.submit(e => {
+  e.preventDefault();
+  socket.emit('new user', $nickname.val(), data => {
+    if (data){
+      $('#nickWrap').hide();
+      $('#contentWrap').show();
+    } else {
+      $nickError.html(`
+        <div class="alert alert-danger">
+          that username already exits.
+        </div>
+      `)
+    }
+    $nickname.val('');
+  })
+});
+// Eventos
 $messageform.submit(function (e) {
   e.preventDefault(); //es para que no refresque la pagina
   socket.emit('send message', $messageBox.val()); // envia el msg a los clientes
@@ -16,7 +39,16 @@ $messageform.submit(function (e) {
 });
 
 socket.on('nuevo mensaje', function (data) {
-  $chat.append(data + '<br/>');
+  $chat.append('<b>' + data.nick + '</b>: ' + data.msg  + '<br/>' );
+});
+// Recorre el array para mostar los usuarios en pantalla 
+socket.on('usernames', data => {
+  let html = '';
+  for (let i = 0; i < data.length; i++) {
+    html += `<p><i class="fas fa-user"></i>${data[i]}</P>`
+  }
+  $users.html(html);
+
 });
 
 })
--- src/public/sockets.js
+++ src/public/sockets.js
@@ -1,14 +1,50 @@
-
+// ESTA ES LA PARTE DEL SERVIDOR
 //ahora quedate escuchando cuando halla otra conexion
-
 module.exports = function(io) { 
+
+   let nicknames = [
+     'many',
+     'renee',
+     'coco'
+   ]; 
 
   io.on('connection', socket => {
     console.log('new usuario conectadoooooooooooooo');
 
-    socket.on('send message', function(data){
-      io.sockets.emit('nuevo mensaje', data);
+
+    socket.on('new user', (data, cb)=> {
+      console.log(data);
+      if (nicknames.indexOf(data) != -1 ) {
+        cb(false);
+      } else {
+        cb(true);
+        socket.nickname = data;
+        nicknames.push(socket.nickname);
+        // io.sockets.emit('usernames', nicknames);
+        updateNicknames();
+       
+      }
     });
 
+
+    socket.on('send message', data => {
+      io.sockets.emit('nuevo mensaje', {
+        msg: data,
+        nick: socket.nickname
+      });
+    });
+
+
+    //CUANDO UN USUARIO SALE DE SESION LO BORRA DE PANTALLA
+    socket.on('disconnect', data => {
+      if(!socket.nickname) return;
+      nicknames.splice(nicknames.indexOf(socket.nickname) ,1);
+      updateNicknames();
+    });
+
+// Elimina un elemento del arry nickname
+    function updateNicknames() {
+      io.sockets.emit('usernames', nicknames);
+    }
   });
 }
